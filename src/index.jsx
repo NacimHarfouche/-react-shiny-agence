370bf2d78a198b84edf3f97bc30c924002ee483d
NacimHarfouche <nacim_4@hotmail.fr>
2022-08-07 23:17:46 +0200

    add some pages and components

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { render } from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Survey from './pages/Survey'
 import Header from './components/Header'
+import Error from './components/Error'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+const root = document.getElementById('root')
+
+render(
   <React.StrictMode>
     <Router>
       <Header />
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/survey">
-        <Survey />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey:questionNumber">
+          <Survey />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+        <Route>
+          <Results path="/results" />
+        </Route>
+        <Route>
+          <Freelances path="/freelances" />
+        </Route>
+      </Switch>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 )

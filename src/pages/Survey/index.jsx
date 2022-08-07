75049adee9e2108df005c6de8e04de697a8a1c89
NacimHarfouche <nacim_4@hotmail.fr>
2022-08-08 00:10:25 +0200

    add func in survey

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,12 +1,17 @@
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 function Survey() {
   const { questionNumber } = useParams()
+
+  const addOne = () => questionNumber + 1
+  const removeOne = () => questionNumber - 1
 
   return (
     <div>
       <h1>Questionnaire 🧮</h1>
       <h2>Question {questionNumber}</h2>
+      <Link to={'/survey/' + removeOne()}>Précédent</Link>
+      <Link to={'/survey/' + addOne()}>Suivant</Link>
     </div>
   )
 }
